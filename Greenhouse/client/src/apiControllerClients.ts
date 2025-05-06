3f3a289dbefaa6b1eb3b75814faa6eb31d814d95
alekss <anoviconoks@gmail.com>
2025-05-06 20:35:46 +0200

    ....

--- Greenhouse/client/src/apiControllerClients.ts
+++ Greenhouse/client/src/apiControllerClients.ts
@@ -1,11 +1,16 @@
 import {AuthClient, SubscriptionClient, WeatherStationClient,SensorClient, ThresholdsClient} from "./generated-client.ts";
 console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
-const baseUrl = import.meta.env.VITE_API_BASE_URL
-const prod = import.meta.env.PROD
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+const prod = import.meta.env.PROD;
 
-export const subscriptionClient = new SubscriptionClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
-export const sensorDataClient = new SensorClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
-export const weatherStationClient = new WeatherStationClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
-export const authClient = new AuthClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
-export const thresholdsClient = new ThresholdsClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
+function ensureProtocol(url: string) {
+  if (url.startsWith("http")) return url;
+  return (prod ? "https://" : "http://") + url;
+}
+
+export const subscriptionClient = new SubscriptionClient(ensureProtocol(baseUrl));
+export const sensorDataClient = new SensorClient(ensureProtocol(baseUrl));
+export const weatherStationClient = new WeatherStationClient(ensureProtocol(baseUrl));
+export const authClient = new AuthClient(ensureProtocol(baseUrl));
+export const thresholdsClient = new ThresholdsClient(ensureProtocol(baseUrl));
 console.log("authClient:", authClient);
